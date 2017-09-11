6053b0909055373c966125433c03578f7e3becd8
Shashikant86 <shashikant.jagtap@icloud.com>
2017-09-11 22:19:10 +0100

    Initial Commit

---
+++ Carthage-CI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carthage-CI
+//
+//  Created by Shashikant Jagtap on 11/09/2017.
+//  Copyright © 2017 Shashikant Jagtap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
